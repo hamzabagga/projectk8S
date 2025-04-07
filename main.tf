6cbc2d01537f8890c827f692bf419dd8f90d78cc
hamzabagga <bagga.hamza@yahoo.fr>
2025-04-07 16:46:52 +0100

    your commit message

--- main.tf
+++ main.tf
@@ -4,3 +4,102 @@ resource "aws_vpc" "main2" {
     Name = "hamza-vpc"
   }
 }
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main2.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "hamza-private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main2.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "hamza-private-subnet-2"
+  }
+}
+
+# Create an Internet Gateway for public subnets
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main2.id
+
+  tags = {
+    Name = "hamza-igw"
+  }
+}
+
+# Create a public route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main2.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+
+  tags = {
+    Name = "hamza-public-route-table"
+  }
+}
+
+# Associate public subnets with the public route table
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Create two public subnets
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.main2.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "hamza-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.main2.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "hamza-public-subnet-2"
+  }
+}
+
+# Create a private route table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main2.id
+
+  tags = {
+    Name = "hamza-private-route-table"
+  }
+}
+
+# Associate private subnets with the private route table
+resource "aws_route_table_association" "private_subnet_1_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_association" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
