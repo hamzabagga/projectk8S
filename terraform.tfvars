1fd4e462d7427f052f0f9fb7c434bfbe65d96bb0
hamzabagga <bagga.hamza@yahoo.fr>
2025-04-11 15:38:53 +0100

    Fix YAML workflows Apply & Destroy

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+vpc_name = "hamza-vpc"
+cidr_block = "10.0.0.0/16"
--- variables.tf
+++ variables.tf
@@ -1,9 +1,6 @@
 variable "vpc_name" {
-  type        = string
-  default     = "hamza-vpc"
-  
+  type        = string  
 }
 variable "cidr_block" {
   type = string
-  default = "10.0.0.0/16"
-}
+  }
